18f0e65827eb02a022e2f3e5eedbd9d620d60d6e
mhoyer <mathieuhoyer@gmail.com>
2024-04-18 15:17:32 +0200

    push

--- src/components/ReposCarousel.tsx
+++ src/components/ReposCarousel.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from '@/components/ui/card';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import React from 'react';
 import { icons } from '@/icons';
