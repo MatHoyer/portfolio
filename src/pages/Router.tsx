949b127a45238e5aa3299fc9283281af99f06620
mhoyer <mathieuhoyer@gmail.com>
2024-04-18 15:21:34 +0200

    push

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -6,13 +6,13 @@ import { ReposList } from './ReposList';
 import { Repo } from './Repo';
 
 export const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="repos" element={<ReposList />} />
-            <Route path="repo/:repoName" element={<Repo />} />
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/portfolio" element={<Home />} />
+      <Route path="/portfolio/profile" element={<Profile />} />
+      <Route path="/portfolio/repos" element={<ReposList />} />
+      <Route path="/portfolio/repo/:repoName" element={<Repo />} />
+      <Route path="/portfolio/*" element={<NotFound />} />
+    </Routes>
+  );
 };
