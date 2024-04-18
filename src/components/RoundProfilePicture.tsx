dfc42b22f9928f025b198e883280f1e31ca7c036
mhoyer <mathieuhoyer@gmail.com>
2024-04-18 15:30:06 +0200

    push

--- src/components/RoundProfilePicture.tsx
+++ src/components/RoundProfilePicture.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 
 export const RoundProfilePicture: React.FC<{ size?: string }> = ({ size }) => {
   return (
-    <Avatar className={cn((size === 'big' && 'w-64 h-64 rounded-full') || 'w-20 h-20 rounded-full')}>
+    <Avatar className={cn(size === 'big' ? 'w-64 h-64 rounded-full' : 'w-20 h-20 rounded-full')}>
       <AvatarImage src="https://github.com/mathoyer.png" alt="profile picture" />
       <AvatarFallback>...</AvatarFallback>
     </Avatar>
