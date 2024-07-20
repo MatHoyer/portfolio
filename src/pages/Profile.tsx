6c82f765435fd38533bea8aadd052ebedde17e9b
MatHoyer <mathieuhoyer@gmail.com>
2024-07-20 21:54:23 +0200

    clean

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,14 +4,6 @@ import { Button } from '@/components/ui/button';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const ProgressBar: React.FC<{ percentage: number }> = ({ percentage }) => {
-  return (
-    <div className="w-full bg-indigo-200 rounded-lg">
-      <div className="h-2 bg-indigo-700 rounded-lg" style={{ width: `${percentage}%` }}></div>
-    </div>
-  );
-};
-
 export const Profile = () => {
   const globalData = useSelector((state: RootStateRepos) => state.repos);
 
@@ -27,12 +19,6 @@ export const Profile = () => {
       </p>
       <div className="flex flex-col space-y-2">
         <SkillChart />
-        {/* {globalData.languagesCount.map((lang) => (
-          <div key={lang.name} className="grid grid-cols-2 items-center space-x-4">
-            <p className="text-right">{lang.name}</p>
-            <ProgressBar percentage={lang.percentage} />
-          </div>
-        ))} */}
       </div>
       <div className="space-x-4">
         <Button variant={'outline'} asChild>
